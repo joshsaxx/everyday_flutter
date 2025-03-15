ae663627c9fe74285310968024da4daab40ff1ba
joshsaxx <joshnkannan@gmail.com>
2025-03-15 22:37:23 +0000

    tutorial_sessions -- bottom nav example part 2 implemented

--- lib/app/tutorial_sessions/bottom_navigation/bottom_nav.dart
+++ lib/app/tutorial_sessions/bottom_navigation/bottom_nav.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tuts/app/tutorial_sessions/navigation_drawer/dafaults/defaults.dart';
 
 class BottomNavPage extends StatefulWidget {
   const BottomNavPage({super.key});
@@ -8,7 +9,6 @@ class BottomNavPage extends StatefulWidget {
 }
 
 class _BottomNavPageState extends State<BottomNavPage> {
-
   final pages = [
     const Center(
       child: Text('Inbox'),
@@ -29,12 +29,51 @@ class _BottomNavPageState extends State<BottomNavPage> {
       child: Text('Spam'),
     ),
   ];
-  
-  var indexClicked = 0;
 
+  var indexClicked = 0;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue[400],
+        title: const Text("Bottom Navigation"),
+      ),
+      body: pages[indexClicked],
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.blue[100],
+        type: BottomNavigationBarType.shifting,
+        //with shifting, gotta set a backgroundColor for each BottomNavigationBarItem and comment out the backgroundColor for BottomNavigationBar
+        //type: BottomNavigationBarType.shifting,
+        elevation: 60,
+        selectedItemColor: Defaults.drawerItemSelectedColor,
+        unselectedItemColor: Defaults.drawerItemColor,
+        currentIndex: indexClicked,
+        onTap: (value) {
+          setState(() {
+            indexClicked = value;
+          });
+        },
+        items: [
+        BottomNavigationBarItem(
+            icon: Icon(Defaults.drawerItemIcon[0]),
+            label: Defaults.drawerItemText[0]),
+        BottomNavigationBarItem(
+            icon: Icon(Defaults.drawerItemIcon[1]),
+            label: Defaults.drawerItemText[1]),
+        BottomNavigationBarItem(
+            icon: Icon(Defaults.drawerItemIcon[2]),
+            label: Defaults.drawerItemText[2]),
+        BottomNavigationBarItem(
+            icon: Icon(Defaults.drawerItemIcon[3]),
+            label: Defaults.drawerItemText[3]),
+        BottomNavigationBarItem(
+            icon: Icon(Defaults.drawerItemIcon[4]),
+            label: Defaults.drawerItemText[4]),
+        /* BottomNavigationBarItem(
+            icon: Icon(Defaults.drawerItemIcon[5]),
+            label: Defaults.drawerItemText[5]), */
+      ]),
+    );
   }
 }
