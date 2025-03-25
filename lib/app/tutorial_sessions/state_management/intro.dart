b24adc1e369b1d39ad2f48b8a151b66a371abd0f
joshsaxx <joshnkannan@gmail.com>
2025-03-25 22:37:45 +0000

    tutorial_sessions -- State Management -- Using setState

--- lib/app/tutorial_sessions/state_management/intro.dart
+++ lib/app/tutorial_sessions/state_management/intro.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
 
-class StateManagementMainPage extends StatelessWidget {
+class StateManagementMainPage extends StatefulWidget {
+
   const StateManagementMainPage({super.key});
+
+  @override
+  State<StateManagementMainPage> createState() => _StateManagementMainPageState();
+}
+
+class _StateManagementMainPageState extends State<StateManagementMainPage> {
+  String data = 'Josh Saxx';
+  void changeValue (String dataChanged){
+    setState(() {
+      data = dataChanged;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     print('Building MainPage');
@@ -9,37 +23,50 @@ class StateManagementMainPage extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.blue[400],
         foregroundColor: Colors.white,
-        title: Text('Data Here'),
+        title: Text(data),
       ),
-      body: Screen2(),
+      body: Screen2(data: data,changeData: changeValue),
     );
   }
 }
 
 class Screen2 extends StatelessWidget {
-  const Screen2({super.key});
+  final String data;
+  final Function(String) changeData;
+
+  Screen2({super.key, required this.data, required this.changeData});
   @override
   Widget build(BuildContext context) {
     print('Building Screen2');
     return Container(
-      child: Screen3(),
+      child: Screen3(data: data,changeData: changeData,),
     );
   }
 }
 
 class Screen3 extends StatelessWidget {
-  const Screen3({super.key});
+  final String data;
+  final Function(String) changeData;
+  const Screen3({super.key, required this.data, required this.changeData});
   @override
   Widget build(BuildContext context) {
     print('Building Screen3');
     return Container(
-      child: Screen4(),
+      child: Screen4(data: data,changeData: changeData,),
     );
   }
 }
 
-class Screen4 extends StatelessWidget {
-  const Screen4({super.key});
+class Screen4 extends StatefulWidget {
+  final String data;
+  final Function(String) changeData;
+  const Screen4({super.key,required this.data,required this.changeData});
+
+  @override
+  State<Screen4> createState() => _Screen4State();
+}
+
+class _Screen4State extends State<Screen4> {
   @override
   Widget build(BuildContext context) {
     print('Building Screen4');
@@ -47,7 +74,7 @@ class Screen4 extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text('Data Here'),
+          Text(widget.data),
           ElevatedButton(
             style: ElevatedButton.styleFrom(
                 backgroundColor: Colors.blue[400],
@@ -56,7 +83,9 @@ class Screen4 extends StatelessWidget {
                   borderRadius: BorderRadius.circular(5)
                 )
               ),
-            onPressed: () {},
+            onPressed: () {
+              widget.changeData('John Doe');
+            },
             child: Text('Change data'),
           ),
         ],
